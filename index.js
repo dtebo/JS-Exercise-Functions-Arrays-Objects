9dbf09e4f1b36c4dd583a2eef2762315b6a0deb7
Darren Tebo <darrentebo83@outlook.com>
2020-04-20 23:03:39 -0400

    twelve tests pass

--- index.js
+++ index.js
@@ -239,6 +239,7 @@ function get3rdCar(inventory) {
 */
 function getCarInfoByIndex(inventory, index) {
   /* code here */
+  return `This is a ${inventory[index].car_make} ${inventory[index].car_model}`;
 }
 
 /**
